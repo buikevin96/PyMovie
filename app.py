ccf5ca6c739c8a74aa40f9ee75ed52b13238be6d
buikevin96 <buikevin96@gmail.com>
2018-07-15 10:51:05 -0400

    Added Tasks to complete and began inital function

---
+++ app.py
@@ -0,0 +1,20 @@
+"""
+- Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit:
+
+- Add Movies
+- See movies
+- Find a movie
+- Stop running the program
+
+Tasks:
+[]: Decide where to store movies
+[]: What is the format of a movie?
+[]: Show the user the main interface and get their input
+[]: Allow users to add movies
+[]: Show all their movies
+[]: Find a movie
+[]: Stop running the program when they type 'q'
+"""
+
+def menu():
+    user_input =("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
