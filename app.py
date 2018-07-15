a16452ba7f89492a09f0b6ab8d6b1afa273a8b34
buikevin96 <buikevin96@gmail.com>
2018-07-15 11:07:22 -0400

    Created initial menu function

--- app.py
+++ app.py
@@ -17,4 +17,25 @@ Tasks:
 """
 
 def menu():
-    user_input =("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
+    user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
+
+    if user_input == 'q':
+        print("Exiting The program...")
+        quit_movie()
+
+    while user_input != 'q':
+        if user_input == 'a':
+            add_movie()
+        elif user_input == 'l':
+            show_movies()
+        elif user_input == 'f':
+            find_movie()
+        else:
+            print("Unknown command-please try again")
+#       elif user_input == 'q':           //This will not run
+#           print("Stopping Program...")  //This will not run
+
+        user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
+
+
+menu()
